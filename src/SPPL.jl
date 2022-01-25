4b0949150005520a97c697a996509af970c09ba3
McCoy Becker <mccoybecker@gmail.com>
2022-01-25 11:55:47 -0500

    Removed PyCall, replace with PythonCall.

--- src/SPPL.jl
+++ src/SPPL.jl
@@ -1,102 +1,118 @@
 module SPPL
 
-using Conda
 using MacroTools
 using MacroTools: @capture, rmlines, unblock
-using PyCall
+using PythonCall
 
-# Install ProbLog.
-if PyCall.conda
-    Conda.add("pip")
-    pip = joinpath(Conda.BINDIR, "pip")
-    run(`$pip install sppl`)
-else
-    try
-        pyimport("sppl")
-        pyimport("sppl.compilers")
-    catch ee
-        typeof(ee) <: PyCall.PyError || rethrow(ee)
-        warn("""
-             Python Dependencies not installed
-             Please either:
-             - Rebuild PyCall to use Conda, by running in the julia REPL:
-             - `ENV[PYTHON]=""; Pkg.build("PyCall"); Pkg.build("Problox")`
-             - Or install the depencences, eg by running pip
-             - `pip install sppl`
-             """
-             )
+# Holds a reference to the objects in
+# the initialized Python runtime.
+struct PythonObject
+    ref::Ref{PythonCall.Py}
+    PythonObject() = new(Ref{PythonCall.Py}())
+end
+
+@doc(
+"""
+    struct PythonObject
+        ref::Ref{Any}
     end
+A safe (pre-allocated) pointer for use with exporting
+loaded Python modules.
+The functionality should match `PyNULL` from `PyCall`.
+""", PythonObject)
+
+function (po::PythonObject)(args...; kwargs...)
+    ref = getfield(po, :ref)
+    return ref[](args...; pairs(kwargs)...)
+end
+
+import Base: getproperty, display
+
+function getproperty(po::PythonObject, s::Symbol)
+    ref = getfield(po, :ref)
+    return getproperty(ref[], s)
+end
+
+function set!(po::PythonObject, a)
+    ref = getfield(po, :ref)
+    ref[] = a
+    return nothing
+end
+
+function display(po::PythonObject)
+    ref = getfield(po, :ref)
+    display(ref[])
 end
 
 # ------------ Precompile compatibility ------------ #
 
-const fractions = PyNULL()
-const sppl = PyNULL()
-const dists = PyNULL()
-const ast_compiler = PyNULL()
-const sppl_compiler = PyNULL()
-const transforms = PyNULL()
-const sym_util = PyNULL()
-const dnf = PyNULL()
-const compiler = PyNULL()
-const Id = PyNULL()
-const IdArray = PyNULL()
-const Skip = PyNULL()
-const Sample = PyNULL()
-const Transform = PyNULL()
-const Cond = PyNULL()
-const IfElse = PyNULL()
-const For = PyNULL()
-const Switch = PyNULL()
-const Sequence = PyNULL()
-const dnf_to_disjoint_union = PyNULL()
-const binspace = PyNULL()
-const Fraction = PyNULL()
-const Sqrt = PyNULL()
+const fractions = PythonObject()
+const sppl = PythonObject()
+const dists = PythonObject()
+const ast_compiler = PythonObject()
+const sppl_compiler = PythonObject()
+const transforms = PythonObject()
+const sym_util = PythonObject()
+const dnf = PythonObject()
+const compiler = PythonObject()
+const Id = PythonObject()
+const IdArray = PythonObject()
+const Skip = PythonObject()
+const Sample = PythonObject()
+const Transform = PythonObject()
+const Cond = PythonObject()
+const IfElse = PythonObject()
+const For = PythonObject()
+const Switch = PythonObject()
+const Sequence = PythonObject()
+const dnf_to_disjoint_union = PythonObject()
+const binspace = PythonObject()
+const Fraction = PythonObject()
+const Sqrt = PythonObject()
 
-function __init__()
+function load_python_deps!()
 
     # ------------ Imports ------------ #
 
-    copy!(fractions, pyimport("fractions"))
-    copy!(sppl, pyimport("sppl"))
-    copy!(dists, pyimport("sppl.distributions"))
-    copy!(ast_compiler, pyimport("sppl.compilers.ast_to_spn"))
-    copy!(sppl_compiler, pyimport("sppl.compilers.sppl_to_python"))
-    copy!(transforms, pyimport("sppl.transforms"))
-    copy!(sym_util, pyimport("sppl.sym_util"))
-    copy!(dnf, pyimport("sppl.dnf"))
-    copy!(compiler, sppl_compiler.SPPL_Compiler)
+    set!(fractions, pyimport("fractions"))
+    set!(sppl, pyimport("sppl"))
+    set!(dists, pyimport("sppl.distributions"))
+    set!(ast_compiler, pyimport("sppl.compilers.ast_to_spn"))
+    set!(sppl_compiler, pyimport("sppl.compilers.sppl_to_python"))
+    set!(transforms, pyimport("sppl.transforms"))
+    set!(sym_util, pyimport("sppl.sym_util"))
+    set!(dnf, pyimport("sppl.dnf"))
+    set!(compiler, sppl_compiler.SPPL_Compiler)
     
     # ------------ Commands ------------ #
 
-    copy!(Id, ast_compiler.Id)
-    copy!(IdArray, ast_compiler.IdArray)
-    copy!(Skip, ast_compiler.Skip)
-    copy!(Sample, ast_compiler.Sample)
-    copy!(Transform, ast_compiler.Transform)
-    copy!(Cond, ast_compiler.Condition)
-    copy!(IfElse, ast_compiler.IfElse)
-    copy!(For, ast_compiler.For)
-    copy!(Switch, ast_compiler.Switch)
-    copy!(Sequence, ast_compiler.Sequence)
+    set!(Id, ast_compiler.Id)
+    set!(IdArray, ast_compiler.IdArray)
+    set!(Skip, ast_compiler.Skip)
+    set!(Sample, ast_compiler.Sample)
+    set!(Transform, ast_compiler.Transform)
+    set!(Cond, ast_compiler.Condition)
+    set!(IfElse, ast_compiler.IfElse)
+    set!(For, ast_compiler.For)
+    set!(Switch, ast_compiler.Switch)
+    set!(Sequence, ast_compiler.Sequence)
 
     # DNF.
-    copy!(dnf_to_disjoint_union, dnf.dnf_to_disjoint_union)
+    set!(dnf_to_disjoint_union, dnf.dnf_to_disjoint_union)
 
     # Utils.
-    copy!(binspace, sym_util.binspace)
-    copy!(Fraction, fractions.Fraction)
-    copy!(Sqrt, transforms.Sqrt)
+    set!(binspace, sym_util.binspace)
+    set!(Fraction, fractions.Fraction)
+    set!(Sqrt, transforms.Sqrt)
 
 end
-__init__()
+__init__() = load_python_deps!()
 
 export fractions, sppl, dists, ast_compiler, sppl_compiler, transforms, sym_util, dnf, compiler
 export Id, IdArray, Skip, Sample, Transform, Cond, IfElse, For, Switch, Sequence
 const array = IdArray
 export array
-set(s) = py"{$s}"; export set
+set(s) = Py(s); export set
 export dnf_to_disjoint_union
 export binspace, Fraction, Sqrt
 
--- src/models.jl
+++ src/models.jl
@@ -1,4 +1,4 @@
 # Accessor utility calls.
-@inline mutual_information(model::PyObject, cond1::PyObject, cond2::PyObject) = model.mutual_information(cond1, cond2)
-@inline condition(model::PyObject, cond::PyObject) = model.condition(cond)
-@inline probability(model::PyObject, cond::PyObject) = model.prob(cond)
+@inline mutual_information(model::Py, cond1::Py, cond2::Py) = model.mutual_information(cond1, cond2)
+@inline condition(model::Py, cond::Py) = model.condition(cond)
+@inline probability(model::Py, cond::Py) = model.prob(cond)
