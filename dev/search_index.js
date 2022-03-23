27314af0d823a9be31294b75e1b80e72fd880438
Documenter.jl <documenter@juliadocs.github.io>
2022-03-23 05:39:08 +0000

    build based on 3c85629

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#API-Documentation","page":"API Documentation","title":"API Documentation","text":"","category":"section"},{"location":"","page":"API Documentation","title":"API Documentation","text":"CurrentModule = SPPL","category":"page"}]
+}
